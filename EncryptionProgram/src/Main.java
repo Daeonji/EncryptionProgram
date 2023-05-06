60e5cd3578d19219f984689729e085b2703fe466
Daeonji <skaterboy198967@gmail.com>
2023-05-05 22:01:22 -0500

    first commit

---
+++ EncryptionProgram/src/Main.java
@@ -0,0 +1,14 @@
+// OTP = One-time Pad
+        /*
+            This encryption technique cannot be cracked,
+            but requires the use of a one-time pre-shared
+            key the same size as, or longer than the message
+            being sent. In this technique, plaintext is paired
+            with a random secret key (also referred to as a One-time Pad).
+         */
+
+public class Main {
+    public static void main(String[] args) {
+        EncryptionProgram ep = new EncryptionProgram();
+    }
+}
